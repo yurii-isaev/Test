237e86a941d8801b9a2852ed7f82b202dda8e18c
Yurii-Isaev <devhouse27@gmail.com>
2021-04-26 19:12:38 +0200

    feat: shared service

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { EmpModalComp } from './components/employee/emp-modal/emp-modal.comp';
 import { DepComp } from './components/department/dep.comp';
 import { DepListComp } from './components/department/dep-list/dep-list.comp';
 import { DepModalComp } from './components/department/dep-modal/dep-modal.comp';
+import { SharedService } from './services/shared/shared.service';
 
 @NgModule({
   declarations: [
@@ -26,7 +27,7 @@ import { DepModalComp } from './components/department/dep-modal/dep-modal.comp';
     HttpClientModule,
     FormsModule,
   ],
-  providers: [],
+  providers: [SharedService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
