016b367a3036be5a6cfeb0dfbd09daa911193a3c
Yurii-Isaev <devhouse27@gmail.com>
2021-05-07 20:17:36 +0200

    test: test call update list service in department list component

--- WebClient/src/app/components/department/dep-list/dep-list.comp.spec.ts
+++ WebClient/src/app/components/department/dep-list/dep-list.comp.spec.ts
@@ -1,23 +1,39 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientModule } from '@angular/common/http';
 import { DepListComp } from './dep-list.comp';
+import { SharedService } from '../../../services/shared/shared.service';
+import { of } from 'rxjs';
 
 describe('DepListComp', () => {
   let component: DepListComp;
   let fixture: ComponentFixture<DepListComp>;
+  let service: SharedService;
+  let spy: jasmine.Spy;
+  let mockList: string[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [DepListComp]
-    }).compileComponents();
+      declarations: [DepListComp],
+      imports: [HttpClientModule]
+    })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(DepListComp);
     component = fixture.componentInstance;
+    service = fixture.debugElement.injector.get<SharedService>(SharedService as any);
+    mockList = [];
+    spy = spyOn(service, 'getDepartmentList').and.returnValue(of(mockList));
     fixture.detectChanges();
   });
 
   it('should create department list component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call shared service when update department list', () => {
+    component.updateDepartmentList();
+    expect(spy.calls.any()).toBeTruthy();
   });
 });
