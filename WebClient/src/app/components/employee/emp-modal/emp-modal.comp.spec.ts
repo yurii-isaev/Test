ca2ac2fd1d2f7b8236029d513907594b31fce170
Yurii-Isaev <devhouse27@gmail.com>
2021-05-17 19:14:42 +0200

    test: test call load department list service in employee modal component

--- WebClient/src/app/components/employee/emp-modal/emp-modal.comp.spec.ts
+++ WebClient/src/app/components/employee/emp-modal/emp-modal.comp.spec.ts
@@ -1,23 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EmpModalComp } from './emp-modal.comp';
+import { SharedService } from '../../../services/shared/shared.service';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { of } from 'rxjs';
+import { IEmployee } from '../emp.comp';
 
 describe('EmpModalComp', () => {
   let component: EmpModalComp;
   let fixture: ComponentFixture<EmpModalComp>;
+  let service: SharedService;
+  let mockList: string[];
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [EmpModalComp]
-    }).compileComponents();
+      declarations: [EmpModalComp],
+      imports: [HttpClientModule, FormsModule]
+    })
+      .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(EmpModalComp);
     component = fixture.componentInstance;
+    component.emp = <IEmployee>{employeeId: 1};
+    service = fixture.debugElement.injector.get<SharedService>(SharedService as any);
+    mockList = [];
     fixture.detectChanges();
   });
 
   it('should create employee modal component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call shared service when load department list', () => {
+    const spy = spyOn(service, 'getAllDepartmentNamesFromDB').and.returnValue(of(mockList));
+    component.loadDepartmentList();
+    expect(spy.calls.any()).toBeTruthy();
   });
 });
