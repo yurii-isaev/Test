2acdd67de8c2ac950dab80cb1386cfad62f7e34d
Yurii-Isaev <devhouse27@gmail.com>
2021-06-04 19:12:33 +0200

    test: test call confirm in employee list component

--- WebClient/src/app/components/employee/emp-list/emp-list.comp.spec.ts
+++ WebClient/src/app/components/employee/emp-list/emp-list.comp.spec.ts
@@ -4,12 +4,14 @@ import { SharedService } from '../../../services/shared/shared.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { of } from 'rxjs';
+import { IEmployee } from '../emp.comp';
 
 describe('EmpListComp', () => {
   let component: EmpListComp;
   let fixture: ComponentFixture<EmpListComp>;
   let service: SharedService;
-  let mockList: string[];
+  let mockList: IEmployee[];
+  let mock: IEmployee;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,6 +26,7 @@ describe('EmpListComp', () => {
     component = fixture.componentInstance;
     service = fixture.debugElement.injector.get<SharedService>(SharedService as any);
     mockList = [];
+    mock = <IEmployee>{employeeId: 1, employeeName: 'test'};
     fixture.detectChanges();
   });
 
@@ -41,5 +44,11 @@ describe('EmpListComp', () => {
     spyOn(service, 'getEmployeeListFromDB').and.returnValue(of(mockList));
     component.updateEmployeeList();
     expect(component.employeeList).toEqual(mockList);
+  });
+
+  it('should call confirm window when show confirm', () => {
+    spyOn(window, 'confirm');
+    component.showConfirmDeleteEmployee(mock);
+    expect(window.confirm).toHaveBeenCalled();
   });
 });
